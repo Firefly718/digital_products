6470b10c67ee9634f765a5250b7bd7853afb5ebf
Firefly718 <System.BLA.16@yandex.ru>
2020-02-11 23:37:31 +0200

    finished

---
+++ js/script.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+  $('.slider-one').slick({
+      dots: true,
+      arrows: true,
+      slidesToShow: 1,
+      autoplay: true,
+      swipeToSlide: true,
+      infinite: true,
+      speed: 500,
+      autoplaySpeed: 6000,
+  });
+   $('.slider-two').slick({
+      arrows: true,
+      nextArrow: '<a href="#" class="arrow arrow--right"></a>',
+      prevArrow: '<a href="#" class="arrow arrow--left"></a>', 
+      slidesToShow: 3,
+      adaptiveHeight: true,
+      autoplay: true,
+      swipeToSlide: true,
+      infinite: true,
+      speed: 500,
+      autoplaySpeed: 6000,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
+
+//------------------------------------------------MAP
+
+function initMap() {
+  let geo = {
+    lat: -7.963919,
+    lng: 112.589311
+  }
+  let markerPosition = {
+    lat: -7.946573,
+    lng: 112.600527
+  }
+
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: geo,
+  });
+
+  let marker = new google.maps.Marker({
+    position: markerPosition,
+    map: map,
+    icon: '../img/marker.png',
+    title: 'Ikan Piranha Atas 220C  Malang - East Java Indonesia'
+  });
+
+
+//--------------------------------------------------FORM -> FILL
+$(".form__input--name").unbind().blur(function() {
+        if ($(".form__input--name").val().length !== 0) {
+          $(".form__star--name").addClass("hide");
+        } else {
+          $(".form__star--name").removeClass("hide");
+        }
+});
+$(".form__input--email").unbind().blur(function() {
+  if ($(".form__input--email").val().length !== 0) {
+            $(".form__star--email").addClass("hide");
+          } else {
+            $(".form__star--email").removeClass("hide");
+        }
+});
+$(".form__text").unbind().blur(function() {
+      if ($(".form__text").val().length !== 0) {
+            $(".form__star--text").addClass("hide");
+          } else {
+            $(".form__star--text").removeClass("hide");
+          }
+});          
+
+
+
+//--------------------------------------------------NAV -> SCROLL
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 300) {
+        $(".nav").addClass("scrolling");
+    } else {
+        $(".nav").removeClass("scrolling");
+    }
+});
+}
